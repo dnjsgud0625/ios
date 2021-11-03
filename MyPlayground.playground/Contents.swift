de4152c0c4860598d8242225980e55841402280c
dnjsgud0625 <dnjsgud0625@naver.com>
2021-11-03 22:56:36 +0900

    var, let

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//변수
+var str = "Hello, playground"
+
+var age = 10
+
+//var str = hello -> ""로 문자열이 묶이지 않아 인식x
+
+age = 20
+age = 30
+age = 55
+
+str = "hi"
+
+ //age = "hi" -> 타입이 안맞아서 오류
+
+// Swift 언어의 특징
+// Safe type 타입 안전한, 강타입, 타입이 엄격하다.
+
+//상수
+let num = 20
+//num = 30 -> 상수의 값은 변하지 않는다.
