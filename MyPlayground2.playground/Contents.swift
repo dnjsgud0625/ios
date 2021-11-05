88c3438ec68241d4dc4dd27be012f164fd642c28
dnjsgud0625 <dnjsgud0625@naver.com>
2021-11-05 22:34:58 +0900

    Type Annotations

---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+//Type Annotation
+
+var str: String = "Hello, playground"
+var myAge: Int = 25
+
+//myAge = "hi" Int 타입으로 선언했으므로 오류
+
+var height: Float = 560
+
+var sum = myAge + Int(height)
+//형변환을 통해 int형으로 바꾸어줌
