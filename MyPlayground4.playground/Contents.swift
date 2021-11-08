4c4d21ed7efc3ab07963d024a514dbc3f9b04d97
dnjsgud0625 <dnjsgud0625@naver.com>
2021-11-08 23:09:46 +0900

    Tuples

---
+++ MyPlayground4.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Tuples
+
+var topTitle = ("메인화면", "mainIcon.png")
+
+//index 접근
+topTitle.0
+topTitle.1
+
+//http status code
+var httpError = (statusCode: 404, description: "not found")
+
+//설명으로 접근
+httpError.statusCode
+httpError.description
