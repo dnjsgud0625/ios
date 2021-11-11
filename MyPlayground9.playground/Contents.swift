279b0eab25d67838c4ea53dc4399c600702b039e
dnjsgud0625 <dnjsgud0625@naver.com>
2021-11-11 23:15:49 +0900

    Operator2

---
+++ MyPlayground9.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//비교 연산자
+//Comparison Operators
+
+let a = 20
+let b = 20
+
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+
+if a>b{
+    print("a is bigger")
+}else{
+    print("a is same or smaller")
+}
+
+//유니코드 a
+"\u{61}"
